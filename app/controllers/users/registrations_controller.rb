8afe9bff12ff3dcd1ecda7ec6883efda73d3a23e
Stephen Richardson <stevierich523@hotmail.com>
2016-05-28 12:08:14 +0000

    Add notice and styles

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,10 +1,11 @@
-class Users: :RegistrationsController < Devise::RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :select_plan, only: :new
 
   def create
     super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
-        if resource.plan_id ==2
+        if resource.plan_id == 2
           resource.save_with_payment
         else
           resource.save
@@ -13,4 +14,11 @@ class Users: :RegistrationsController < Devise::RegistrationsController
     end
   end
 
+  private
+    def select_plan
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
+        flash[:notice] = "Please select a membership plan to sign up."
+        redirect_to root_url
+      end
+    end
 end
